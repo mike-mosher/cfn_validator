ccb610458540a32480cc044fd14af61b895950f4
Mike Mosher <mikmoshe@amazon.com>
2019-04-08 22:04:10 -0500

    updated

--- Test/test_cfn_validator.py
+++ Test/test_cfn_validator.py
@@ -1,5 +1,13 @@
 """Tests for cfn_validator.py"""
 
 import pytest
-from cfn_validator import cfn_validator
+from cfn_validator import ValidateParameters
 
+
+def test_verify_all_template_params_provided_by_input_params():
+    template_params = {}
+    input_params = None
+    no_input_params_error_msg = '\'input_parameter_filename\' not provided.  Exiting.'
+
+    assert ValidateParameters.verify_all_template_params_provided_by_input_params(
+        template_params=template_params, input_params=input_params) == no_input_params_error_msg
